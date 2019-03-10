70c540f99aa1abe1964c714af02b6e4653246dab
JonathanDuncan <duncanjonathan@yahoo.ca>
2019-03-09 22:30:20 -0600

    Add document page and signwriting to project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   {
     path: 'list',
     loadChildren: './list/list.module#ListPageModule'
-  }
+  },
+  { path: 'document', loadChildren: './document/document.module#DocumentPageModule' }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,11 @@ export class AppComponent {
       title: 'List',
       url: '/list',
       icon: 'list'
+    },
+    {
+      title: 'Document',
+      url: '/document',
+      icon: 'document'
     }
   ];
 
