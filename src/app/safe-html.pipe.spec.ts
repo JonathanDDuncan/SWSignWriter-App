5d3fbd31bd65220bf04aae9a307150fda67aa418
JonathanDuncan <duncanjonathan@yahoo.ca>
2019-04-09 19:10:08 -0600

    Mock dependency for SafeHtmlPipe

--- src/app/safe-html.pipe.spec.ts
+++ src/app/safe-html.pipe.spec.ts
@@ -1,8 +1,17 @@
 import { SafeHtmlPipe } from './safe-html.pipe';
+import { DomSanitizer } from '@angular/platform-browser';
+import { TestBed } from '@angular/core/testing';
+
+let sanitizer: DomSanitizer;
+
+beforeEach(() => {
+  TestBed.configureTestingModule({ providers: [DomSanitizer] });
+});
 
 describe('SafeHtmlPipe', () => {
   it('create an instance', () => {
-    const pipe = new SafeHtmlPipe();
+    sanitizer = TestBed.get(DomSanitizer);
+    const pipe = new SafeHtmlPipe(sanitizer);
     expect(pipe).toBeTruthy();
   });
 });
