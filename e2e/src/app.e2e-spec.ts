789ece31e04218984202205a8fca7072e8fcc487
JonathanDuncan <duncanjonathan@yahoo.ca>
2019-04-10 17:38:08 -0600

    e2e tests passing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,23 @@
+import { element, browser } from 'protractor';
 import { AppPage } from './app.po';
+
 
 describe('new App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
+
   });
   const url = '/view';
   describe('default screen', () => {
 
     beforeEach(() => {
       page.navigateTo(url);
+    });
+    it('should have a title', function () {
+      browser.get('/view');
+      expect(browser.getTitle()).toEqual('Ionic App');
     });
     it('should have a title saying Document', () => {
       page.getPageOneTitleText().then(title => {
