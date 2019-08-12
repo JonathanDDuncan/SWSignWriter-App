b8799a286ef5ed4132ed52504b772e63464ba1ed
JonathanDuncan <duncanjonathan@yahoo.ca>
2019-08-11 21:46:25 -0600

    Days left on screen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
+import { Storage } from '@ionic/storage';
+import { TrialService } from './../services/trial.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { StorageService } from '../storage.service';
 
 @Component({
   selector: 'app-home',
@@ -7,10 +10,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+  public daysleft: number;
+  constructor(
+    public router: Router,
+    private trial: TrialService,
+    private storage: StorageService
+  ) { }
 
-  constructor(public router: Router) { }
-
-  ngOnInit() {
+  async ngOnInit() {
+    const profile = await this.storage.GetCurrentUserProfile();
+    this.daysleft = await this.trial.GetTrialDaysLeft(profile.email);
   }
 
   goSubscribe() {
