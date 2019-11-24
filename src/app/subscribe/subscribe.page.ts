9194fbc64575934ff1667a2661b4bad8cb9d760c
JonathanDuncan <JoYoDuncan@gmail.com>
2019-11-23 20:19:43 -0600

    Send data to server

--- src/app/subscribe/subscribe.page.ts
+++ src/app/subscribe/subscribe.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { StorageService } from '../storage.service';
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.page.html',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubscribePage implements OnInit {
 
-  constructor() { }
-
+  constructor(private http: HttpClient, private storage: StorageService  ) { }
+  private serverUrl = 'https://localhost:44309/'
   ngOnInit() {
   }
 
+  async SubscribeMonthly(){
+    const profile = await this.storage.GetCurrentUserProfile();
+    console.log(profile);
+
+    this.http.post(this.serverUrl + 'api/stripe/sendclient', profile, { headers: new HttpHeaders({
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    }) })
+      .subscribe(data => {
+        console.log(data);
+       }, error => {
+        console.log(error);
+      });
+  }
+
+  SubscribeYearly() {
+
+  }
 }
