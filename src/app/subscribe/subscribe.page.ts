a4e21ecd0bf3a0c7d6d3d785e6b6befd656bb6aa
JonathanDuncan <JoYoDuncan@gmail.com>
2019-11-24 07:59:25 -0600

    Create Session

--- src/app/subscribe/subscribe.page.ts
+++ src/app/subscribe/subscribe.page.ts
@@ -9,11 +9,11 @@ import { StorageService } from '../storage.service';
 export class SubscribePage implements OnInit {
 
   constructor(private http: HttpClient, private storage: StorageService  ) { }
-  private serverUrl = 'https://localhost:44309/'
+  private serverUrl = 'https://localhost:44309/';
   ngOnInit() {
   }
 
-  async SubscribeMonthly(){
+  async SubscribeMonthly() {
     const profile = await this.storage.GetCurrentUserProfile();
     const subscriptionEndDate = await this.storage.GetSubscriptionEndDate(profile.email);
     const trialStartDate = await this.storage.GetTrialStartDate(profile.email);
@@ -25,9 +25,9 @@ export class SubscribePage implements OnInit {
       planId: planId,
       subscriptionEndDate: subscriptionEndDate,
       trialStartDate: trialStartDate
-    }
+    };
 
-    this.http.post(this.serverUrl + 'api/stripe/sendclient', subscriptionRequest, { headers: new HttpHeaders({
+    this.http.post(this.serverUrl + 'api/stripe/createsession', subscriptionRequest, { headers: new HttpHeaders({
       'Accept': 'application/json',
       'Content-Type': 'application/json',
     }) })
