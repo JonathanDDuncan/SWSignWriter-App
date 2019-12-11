5dbe0ed16e670f3021d7a07537e8f869fabcd583
JonathanDuncan <JoYoDuncan@gmail.com>
2019-12-10 20:07:08 -0600

    Get Subscription info from server

--- src/app/stripesuccess/stripesuccess.page.ts
+++ src/app/stripesuccess/stripesuccess.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { StorageService } from '../storage.service';
+
 
 @Component({
   selector: 'app-stripesuccess',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stripesuccess.page.scss'],
 })
 export class StripesuccessPage implements OnInit {
+  private serverUrl = 'https://swsignwriterapi.azurewebsites.net/';
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private storage: StorageService
+    ) {}
 
-  ngOnInit() {
+
+  async ngOnInit() {
+    await this.getSubscriptionInfo();
   }
 
+  async getSubscriptionInfo() {
+    const profile = await this.storage.GetCurrentUserProfile();
+      const subscriptionData = {
+        privatekey: '**GSew10o0uJiAg4qpTAvQ$KEMaCjC6P7@su2Dd1C9#a8Y$VISWXzYogPhYk&N6p5&cGb1k@nGFX',
+        email: profile.email
+      };
+
+      this.http.post(this.serverUrl + 'api/stripe/getsubscription', subscriptionData, {
+      headers: new HttpHeaders({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      })
+    });
+  }
 }
