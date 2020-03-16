e5f23098fa31e9a2f9f3d003b49510d456ac5dbc
JonathanDuncan <JoYoDuncan@gmail.com>
2020-03-16 17:31:55 -0600

    Fix bug is subscribed

--- src/app/services/subscription.service.ts
+++ src/app/services/subscription.service.ts
@@ -22,7 +22,7 @@ export class SubscriptionService {
 
   async GetSubscriptionDaysLeft(email: string): Promise<number> {
     const subscriptionEndDate: Date = await this.GetSubscriptionEndDate(email);
-    const diff = Math.abs(subscriptionEndDate.getTime() - new Date().getTime());
+    const diff = Math.abs(new Date(subscriptionEndDate).getTime() - new Date().getTime());
     const daysLeft: number = Math.ceil(diff / (1000 * 3600 * 24));
 
     return (daysLeft > 0) ? daysLeft : 0;
