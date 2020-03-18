8449a303cdb72ec6d3664642c39a1c2e5f65d8ac
JonathanDuncan <JoYoDuncan@gmail.com>
2020-03-17 20:11:21 -0600

    Get subscription when logging in.

--- src/app/callback/callback.component.ts
+++ src/app/callback/callback.component.ts
@@ -1,3 +1,4 @@
+import { StripeService } from './../stripe.service';
 import { StorageService } from './../storage.service';
 import { Component, OnInit, PipeTransform } from '@angular/core';
 import { AuthService } from '../auth.service';
@@ -9,12 +10,17 @@ import { AuthService } from '../auth.service';
 })
 export class CallbackComponent implements OnInit {
 
-  constructor(private auth: AuthService, private storage: StorageService) { }
+  constructor(private auth: AuthService,
+    private storage: StorageService,
+    private stripeService: StripeService
+    ) { }
 
   ngOnInit() {
     this.auth.userProfile$.subscribe(async userProfile => {
+
       if (userProfile) {
         this.storage.SaveCurrentUserProfile(userProfile);
+        this.stripeService.GetandSaveStripeSubscriptionData(userProfile.email);
         const trialDate = await this.storage.GetTrialStartDate(userProfile.email);
         if (!trialDate) {
           this.storage.SaveTrialStartDate(userProfile.email, new Date());
