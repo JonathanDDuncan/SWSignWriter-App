25c90538e01ec09222f20c19ebf95c845fbed6d1
JonathanDuncan <JoYoDuncan@gmail.com>
2020-03-29 16:28:52 -0600

    Social Share

--- src/app/show-image/show-image.page.ts
+++ src/app/show-image/show-image.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { DomSanitizer } from '@angular/platform-browser';
+import { SharePage } from '../share/share.page';
 
 @Component({
   selector: 'app-show-image',
@@ -12,7 +13,7 @@ export class ShowImagePage implements OnInit {
   public contentWidth: Number;
   public contentHeight: Number;
   public swCanvas: HTMLCanvasElement; // your canvas element
-
+  public modalCtrl: HTMLIonModalElement;
   @Input() canvas: HTMLCanvasElement;
   constructor(public modalController: ModalController, private sanitizer: DomSanitizer) { }
 
@@ -29,6 +30,37 @@ export class ShowImagePage implements OnInit {
   getimage() {
     this.contentHeight = this.swCanvas.height / 4;
     this.contentWidth = this.swCanvas.width / 4;
-    return this.sanitizer.bypassSecurityTrustResourceUrl( '' + this.imagebase64);
-}
+    return this.sanitizer.bypassSecurityTrustResourceUrl('' + this.imagebase64);
+  }
+
+  async socialShare() {
+    const navShare = (window.navigator as any);
+    if (navShare.share) {
+      this.canvas.toBlob(
+        function (blob) {
+          const filename = 'signWriting.png';
+          const fd = new FormData();
+          const files = [];
+          fd.append('SignWriting', blob, filename);
+          for (const newfile of fd.getAll('file')) {
+            files.push(newfile);
+          }
+          navShare.share({
+            files: files
+          }).then(() => {
+            console.log('Thanks for sharing!');
+          })
+            .catch(console.error);
+        });
+
+    } else {
+      this.modalCtrl = await this.modalController.create({
+        component: SharePage,
+        componentProps: { canvas: this.canvas }
+      });
+
+      await this.modalCtrl.present();
+      await this.modalCtrl.onDidDismiss();
+    }
+  }
 }
