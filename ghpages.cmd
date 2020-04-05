f0f02c5736b8dbebc2de8ffcad16df902fc01ea0
JonathanDuncan <JoYoDuncan@gmail.com>
2020-04-05 15:06:12 -0600

    Add sentry to deploy scripts

--- ghpages.cmd
+++ ghpages.cmd
@@ -1,2 +1,7 @@
+set SENTRY_AUTH_TOKEN=78e8851c467f4d6ea0ccfd80bb928fd6d9a820bb907c450b9a635359508220c9
+set SENTRY_ORG=swsignwriter
+for /f %%i in ('.\node_modules\.bin\sentry-cli releases propose-version') do set VERSION=%%i
+start .\node_modules\.bin\sentry-cli releases new  -p swsignwriterproj "%VERSION%"
 npm run deploy
+start .\node_modules\.bin\sentry-cli releases finalize -p swsignwriterproj "%VERSION%"
 pause
