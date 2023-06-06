88a69cafda3cdac7668c7b10df785e19a2601b1b
Fernando Martinez <cfmartinez99@gmail.com>
2023-06-06 13:51:31 -0600

    Updates

---
+++ 2680.212310194cc9be42.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[2680],{2680:(t,e,n)=>{n.r(e),n.d(e,{SplashScreenWeb:()=>o});var r=n(5861),s=n(7423);class o extends s.Uw{show(_){return(0,r.Z)(function*(){})()}hide(_){return(0,r.Z)(function*(){})()}}}}]);
